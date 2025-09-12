f5cf65b9ea2b9f57ac1179e83c8b41c0c85e599b
aduh95 <14309773+aduh95@users.noreply.github.com>
2025-09-12 07:41:05 +0000

    tools: update nixpkgs-unstable to d33e926

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,10 +1,10 @@
 arg:
 let
   repo = "https://github.com/NixOS/nixpkgs";
-  rev = "ca77296380960cd497a765102eeb1356eb80fed0";
+  rev = "d33e926c80e6521a55da380a4c4c44a7462af405";
   nixpkgs = import (builtins.fetchTarball {
     url = "${repo}/archive/${rev}.tar.gz";
-    sha256 = "1airrw6l87iyny1a3mb29l28na4s4llifprlgpll2na461jd40iy";
+    sha256 = "1j0ir1f9zv9y674apv7fnmmhr0qf8bjnh7qv6ia47bbs1pzxgr2x";
   }) arg;
 in
 nixpkgs
