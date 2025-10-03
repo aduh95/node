54e7235f578a11c38bf6b69345d647c625e6443c
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-10-03 14:39:53 +0200

    drop! update nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,10 +1,10 @@
 arg:
 let
   repo = "https://github.com/NixOS/nixpkgs";
-  rev = "ca77296380960cd497a765102eeb1356eb80fed0";
+  rev = "1cef900e590ea97fb326b74dfff77d23e5e9f8c1";
   nixpkgs = import (builtins.fetchTarball {
     url = "${repo}/archive/${rev}.tar.gz";
-    sha256 = "1airrw6l87iyny1a3mb29l28na4s4llifprlgpll2na461jd40iy";
+    sha256 = "10460nq82m1gwjcmg36qjbrimi3zpb9wyhwmlbz6ayjgbff8drwj";
   }) arg;
 in
 nixpkgs
