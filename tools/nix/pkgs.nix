f936ab88281b1584c5c18573bd76a558c11e3ce1
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-09-08 11:40:17 +0200

    fixup! build: do not use bundled simdutf when built with `--shared-simdutf`

--- tools/nix/pkgs.nix
+++ tools/nix/pkgs.nix
@@ -2,12 +2,27 @@ arg:
 let
   repo = "https://github.com/NixOS/nixpkgs";
   rev = "e5ead30d0824debba629dcf0720abeddee57b7d6";
-  nixpkgs = import (builtins.fetchTarball {
+  nixpkgs = builtins.fetchTarball {
     url = "${repo}/archive/${rev}.tar.gz";
     sha256 = "0sydiakqjp2qhr8h573bv48ng8928rjpwkx600xmxvsyz8kwczn1";
-  }) arg;
+  };
 in
 # Unstable channel no longer supports Intel architecture for macOS. We can use the 26.05 channel
 # to keep testing on that platform for a little longer.
 # TODO: remove this when 26.05 is EOL (end of 2026)
-if builtins.currentSystem == "x86_64-darwin" then (import ./pkgs-26.05.nix arg) else nixpkgs
+import (if builtins.currentSystem == "x86_64-darwin" then ./pkgs-26.05.nix else nixpkgs) (
+  arg
+  // ({
+    overlays = (arg.overlays or [ ]) ++ [
+      (final: old: {
+        # TODO: remove this once the pin we use has picked up https://github.com/NixOS/nixpkgs/pull/557405
+        simdutf = old.simdutf.overrideAttrs (
+          old:
+          final.lib.optionalAttrs (final.lib.versionAtLeast "8.0.0" old.version) {
+            cmakeFlags = old.cmakeFlags ++ [ (final.lib.cmakeFeature "SIMDUTF_CXX_STANDARD" "20") ];
+          }
+        );
+      })
+    ];
+  })
+)
